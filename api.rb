e1b97149eea910fe608ac9812bc87be08eaa4702
JpnLavender <nagisa20000014@gmail.com>
2016-08-27 19:06:14 +0900

    Debug string -> integer

--- api.rb
+++ api.rb
@@ -27,5 +27,5 @@ post '/boards/:board_name/texts.json' do
 end
 
 delete '/boards/:board_name/stamps/:text_id.json' do
-  PutText.find_by(text_id: params[:text_id], board_id: params[:board_name])
+  PutText.find_by(text_id: params[:text_id].to_i, board_id: params[:board_name])
 end
