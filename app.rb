8938c4c25f12db59a24115d13e1d1487b7801d1d
JpnLavender <nagisa20000014@gmail.com>
2016-08-27 16:53:27 +0900

    Add LoginSystem

--- app.rb
+++ app.rb
@@ -17,6 +17,40 @@ get '/' do
   end
 end
 
+get '/sign_up' do
+  @form_action = '/sign_up'
+  erb :sign
+end
+
+post '/sign_up' do
+  session[:user_token] = SecureRandom.uuid
+  User.create(mail: params[:mail], token: session[:user_token], password: params[:password], password_confirmation: params[:password])
+  redirect '/admin_page'
+end
+
+get '/sign_in' do
+  @form_action = '/sign_in'
+  erb :sign
+end
+
+post '/sign_in' do
+  user = User.find_by_mail(mail: params[:mail])
+  if user && user.authenticate(params[:password])
+    session[:user_token] = user.token
+    redirect '/admin_page'
+  else
+    redirect '/sign_up'
+  end
+end
+
+get '/admin_page' do
+  if User.find_by(session[:user_token])
+    erb :admin_page
+  else
+    redirect '/sign_in'
+  end
+end
+
 post '/' do
   session[:id] = SecureRandom.uuid
   Board.create(name: session[:id], screen_name: params[:board_name])
