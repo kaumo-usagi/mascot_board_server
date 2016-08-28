bb07db48a255c34f49aca43e8a00982c3847a299
JpnLavender <nagisa20000014@gmail.com>
2016-08-28 13:57:03 +0900

    [Add] get stamp.json

--- api.rb
+++ api.rb
@@ -1,6 +1,18 @@
 require 'padrino-helpers'
 require 'kaminari/sinatra'
 
+get '/boards/:board_name/stamps.json' do
+  stamps = Board.find_by(name: params[:board_name]).put_stamps
+  stamps.map { |stamp| 
+    { 
+      id:       stamp.id,
+      stamp_id: stamp.stamp_id, 
+      x:        stamp.x, 
+      y:        stamp.y, 
+      url:      stamp.stamps.url
+    } }
+end
+
 post '/boards/:board_name/stamps.json' do
   data = JSON.parse(request.body.read)
   stamps = PutStamp.new(
@@ -16,6 +28,9 @@ delete '/boards/:board_name/stamps/:stamp_id.json' do
   PutStamp.where(stamp_id: params[:stamp_id].to_i, board_id: params[:board_name].to_i)[0].delete
 end
 
+
+get '/boards/:board_name/texts.json' do
+end
 
 post '/boards/:board_name/texts.json' do
   board = Board.find_by(name: params[:board_name])
