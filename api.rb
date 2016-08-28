8fe7227d2a11dc48cc589ccceeb88f7c2d54c054
JpnLavender <nagisa20000014@gmail.com>
2016-08-28 14:32:06 +0900

    get stamps.json & text.json in if fork

--- api.rb
+++ api.rb
@@ -13,19 +13,23 @@ end
 
 post '/boards/:board_name/stamps.json' do
   board = Board.find_by(name: params[:board_name])
-  data = JSON.parse(request.body.read)
-  put_stamp = PutStamp.new(
-    board_id: board.id,
-    stamp_id: data["stamp_id"], 
-    x:        data["x"], 
-    y:        data["y"]
-  )
-  if put_stamp.save
-    stamp = put_stamp.stamp
-    res_body = { id: put_stamp.id, stamp_id: stamp.id, x: put_stamp.x, y: put_stamp.y, url: stamp.data.url }
-    redis = EM::Hiredis.connect
-    redis.publish("boards::#{board.id}::image::put", res_body.to_json)
-    status 204
+  if board
+    data = JSON.parse(request.body.read)
+    put_stamp = PutStamp.new(
+      board_id: board.id,
+      stamp_id: data["stamp_id"], 
+      x:        data["x"], 
+      y:        data["y"]
+    )
+    if put_stamp.save
+      stamp = put_stamp.stamp
+      res_body = { id: put_stamp.id, stamp_id: stamp.id, x: put_stamp.x, y: put_stamp.y, url: stamp.data.url }
+      redis = EM::Hiredis.connect
+      redis.publish("boards::#{board.id}::image::put", res_body.to_json)
+      status 204
+    else
+      status 400
+    end
   else
     status 400
   end
@@ -48,17 +52,24 @@ end
 
 post '/boards/:board_name/texts.json' do
   board = Board.find_by(name: params[:board_name])
-  data = JSON.parse(request.body.read)
-  text = PutText.new(
-    board_id: board.id,
-    body:     data["body"],
-    x:        data["x"], 
-    y:        data["y"]
-  )
-  if text.save
-    redis = EM::Hiredis.connect
-    redis.publish("boards::#{board.id}::text::put", text.to_json)
-    status 204
+  if board
+    data = JSON.parse(request.body.read)
+    puts "=" * 30
+    p data["body"]
+    puts "+" * 30
+    text = PutText.new(
+      board_id: board.id,
+      body:     data["body"],
+      x:        data["x"], 
+      y:        data["y"]
+    )
+    if text.save
+      redis = EM::Hiredis.connect
+      redis.publish("boards::#{board.id}::text::put", text.to_json)
+      status 204
+    else
+      status 400
+    end
   else
     status 400
   end
